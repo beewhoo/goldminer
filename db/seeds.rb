c4703ebec0917b33d7d34bc09778a1f469562970
bibek <bibeksinghthapa@gmail.com>
2021-02-20 00:35:25 -0500

    db seed - seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+TAGS = %w(Lead HighValue Finalized Stage2 LowValue Churned RealEstate)
+
+if Rails.env.development?
+
+    Tagging.destroy_all
+    Tag.destroy_all
+    Contact.destroy_all
+
+  contacts = []
+  tags = []
+
+ puts '-- Create contacts --'
+
+  10.times do |i|
+    contacts << FactoryBot.create(:contact)
+  end
+
+ puts '-- Create tags --'
+
+  TAGS.each do | tag |
+    tags << FactoryBot.create(:tag, name: tag )
+  end
+
+  puts '-- Create taggings --'
+  contacts.each do  |contact|
+    rand(1..3).times do
+      Tagging.find_or_create_by(contact_id: contact.id, tag_id: tags.sample.id)
+    end
+  end
+
+  puts '-- Seeding completed --'
+
+
+end
