708317aa350283668c37ae937a8eec7c45d07b4d
bibek <bibeksinghthapa@gmail.com>
2021-02-20 00:42:47 -0500

    tag - callback sanatize name to snake case before save

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,17 @@ class Tag < ApplicationRecord
   #relationships
   has_many :taggings
   has_many :contacts, through: :taggings
+  #callback
+  before_save :snake_case_name, if: :new_record_or_name_changed?
+
+  def snake_case_name
+    self.name = name.underscore
+  end
+
+  private
+
+  def new_record_or_name_changed?
+    new_record? || name_changed?
+  end
+
 end
