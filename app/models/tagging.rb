c4d46087613edc02092cdc4623831906c5ff8693
bibek <bibeksinghthapa@gmail.com>
2021-02-19 20:21:25 -0500

    tagging - migraiton for tagging table & created tagging model

---
+++ app/models/tagging.rb
@@ -0,0 +1,5 @@
+class Tagging < ApplicationRecord
+  #relationships
+  belongs_to :tag
+  belongs_to :contact
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_010148) do
+ActiveRecord::Schema.define(version: 2021_02_20_010857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2021_02_20_010148) do
     t.index ["email"], name: "index_contacts_on_email", unique: true
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "tag_id", null: false
+    t.bigint "contact_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["contact_id"], name: "index_taggings_on_contact_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.citext "name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -32,4 +41,6 @@ ActiveRecord::Schema.define(version: 2021_02_20_010148) do
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
+  add_foreign_key "taggings", "contacts"
+  add_foreign_key "taggings", "tags"
 end
