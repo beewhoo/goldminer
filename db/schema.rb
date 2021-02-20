fe1f7640e7275a99a773876b40d9de4b2100c0a1
bibek <bibeksinghthapa@gmail.com>
2021-02-19 20:07:39 -0500

    tag - migration for tag table and created tag model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_004956) do
+ActiveRecord::Schema.define(version: 2021_02_20_010148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2021_02_20_004956) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_contacts_on_email", unique: true
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.citext "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
 end
